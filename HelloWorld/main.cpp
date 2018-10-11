2c8e7e479f7a74e6fd21310483b8399ab1b4379e
vikrantbasotra <basotra.vikrant@gmail.com>
2018-10-10 19:35:25 -0500

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Vikrant Basotra on 10/10/18.
+//  Copyright © 2018 Vikrant Basotra. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
